03db3f8871088efbda9695e325fe8628aa09c381
sonofwinds <18939636972@163.com>
2019-02-27 14:01:35 +0800

    spring MVC demo

--- src/main/java/com/bj58/controller/HelloController.java
+++ src/main/java/com/bj58/controller/HelloController.java
@@ -1,8 +1,9 @@
 package com.bj58.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class HelloController {
     @RequestMapping(value="/Hello")  
     public String HelloWorld(Model model){  
